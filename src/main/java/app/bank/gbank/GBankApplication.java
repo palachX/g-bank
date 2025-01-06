0e8fc97cbc65184afa5258f8b29223d5c20baec4
Алексей <gurski.alex@gmail.com>
2025-01-06 12:16:16 +0300

    Init app

---
+++ src/main/java/app/bank/gbank/GBankApplication.java
@@ -0,0 +1,13 @@
+package app.bank.gbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GBankApplication.class, args);
+    }
+
+}
